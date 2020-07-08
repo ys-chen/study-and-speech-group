66f458ca189c563a19ec6fa05b9d349176911b67
Nikki Chen <yangsyuan8012@gmail.com>
2020-07-08 15:07:57 +0800

    Updates

---
+++ precache-manifest.8ab0b8e2c1db225589ad14c11668e7a8.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a8cf8194f3fc4924ff8c7aae41eda5",
+    "url": "/study-and-speech-group/index.html"
+  },
+  {
+    "revision": "962ee5df9135394f98f4",
+    "url": "/study-and-speech-group/static/css/2.34d457cb.chunk.css"
+  },
+  {
+    "revision": "ced681f6f88e24f13530",
+    "url": "/study-and-speech-group/static/css/main.0365e080.chunk.css"
+  },
+  {
+    "revision": "962ee5df9135394f98f4",
+    "url": "/study-and-speech-group/static/js/2.7ebe4a17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/study-and-speech-group/static/js/2.7ebe4a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced681f6f88e24f13530",
+    "url": "/study-and-speech-group/static/js/main.d9d4d530.chunk.js"
+  },
+  {
+    "revision": "3ff85d4dcde7d1233971",
+    "url": "/study-and-speech-group/static/js/runtime-main.0bbe16fd.js"
+  },
+  {
+    "revision": "19b356436126b58f116b2feae0c0971f",
+    "url": "/study-and-speech-group/static/media/NotoSansTC-Black.19b35643.otf"
+  },
+  {
+    "revision": "998c7dda33d799f3e7d02d2f98d775c2",
+    "url": "/study-and-speech-group/static/media/NotoSansTC-Bold.998c7dda.otf"
+  },
+  {
+    "revision": "48c0572b995c73023fa5126531875f27",
+    "url": "/study-and-speech-group/static/media/NotoSansTC-Light.48c0572b.otf"
+  },
+  {
+    "revision": "7a7f0d791466529c791ff2a52458327f",
+    "url": "/study-and-speech-group/static/media/NotoSansTC-Medium.7a7f0d79.otf"
+  },
+  {
+    "revision": "8b34790154f7ba7b9d229207c4a2f356",
+    "url": "/study-and-speech-group/static/media/NotoSansTC-Regular.8b347901.otf"
+  },
+  {
+    "revision": "b9270c2463e79876d02564d7b2ad1218",
+    "url": "/study-and-speech-group/static/media/NotoSansTC-Thin.b9270c24.otf"
+  },
+  {
+    "revision": "fa6493a745209f9f7204753dccd83ee9",
+    "url": "/study-and-speech-group/static/media/avatar.fa6493a7.png"
+  },
+  {
+    "revision": "f326a532187d9873d0d40c2662104383",
+    "url": "/study-and-speech-group/static/media/banner.f326a532.jpg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/study-and-speech-group/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/study-and-speech-group/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/study-and-speech-group/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/study-and-speech-group/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/study-and-speech-group/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/study-and-speech-group/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/study-and-speech-group/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/study-and-speech-group/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/study-and-speech-group/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/study-and-speech-group/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/study-and-speech-group/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/study-and-speech-group/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/study-and-speech-group/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/study-and-speech-group/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/study-and-speech-group/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "e0eab74d48c6d753ef9bc506d665f376",
+    "url": "/study-and-speech-group/static/media/made-with-bulma.e0eab74d.png"
+  }
+]);
