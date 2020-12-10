30837a83e75472c2bcfffdeb861aff89d1e5d709
Nikki Chen <yangsyuan8012@gmail.com>
2020-12-10 19:06:21 +0800

    update web structure
    - 2020Q4 info

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
       {/* Hero title */}
       <section className="hero is-bold has-text-centered banner">
         <div className="hero-body">
-          <div className="py-5 border-box">
+          <div className="py-5 border-box container">
             <div className="title is-size-1">讀書分享會</div>
             <div className="subtitle">多一點時間，聆聽一本好書</div>
           </div>
@@ -45,12 +45,12 @@ function App() {
         <div className="container">
           <Title>基本資訊</Title>
           <div className="columns">
-            <InfoCard title="參與前">
+            <InfoCard>
               <ul>
-                <li>時間：2020/10/14 (三) 20:00-21:00 PM</li>
+                <li>時間：2020/12/29 (二) 20:30-21:30 PM</li>
                 <li>類型：線上讀書會</li>
                 <li>
-                  工具：開場前十分鐘發送會議代碼，聽眾自行登入
+                  工具：開場前半小時發送會議連結，聽眾自行登入
                   <a href="https://meet.google.com/"> Google Meet </a>
                   。
                 </li>
@@ -65,30 +65,6 @@ function App() {
                   <a href="https://www.google.com/gmail/"> Gmail </a>
                   亦可）
                 </li>
-              </ul>
-            </InfoCard>
-            <InfoCard title="參與中">
-              <ul>
-                <li>不發出其他聲響干擾演講者，如播音樂。</li>
-                <li>中途離席不需告知。</li>
-                <li>
-                  對每位講者至少
-                  <span className="tag is-warning is-light is-medium px-0">提出一個問題</span>
-                  或是自身
-                  <span className="tag is-warning is-light is-medium px-0">經驗分享</span>
-                  。
-                </li>
-              </ul>
-            </InfoCard>
-            <InfoCard title="參與後">
-              <ul>
-                <li>
-                  填寫
-                  <span className="tag is-warning is-light is-medium px-0">回饋表單</span>
-                  ，感謝參與。
-                </li>
-                <li>表單連結將在讀書會結束後發送。</li>
-                <li>報名下次讀書分享會請洽 Niko。</li>
               </ul>
             </InfoCard>
           </div>
@@ -109,44 +85,76 @@ function App() {
           </div>
         </div>
       </section>
-      {/* Agenda */}
+      {/* Notice */}
       <section className="section">
         <div className="container">
-          <Title>議程</Title>
-          <table className="table" style={{ width: '100%' }}>
-            <thead>
-              <tr>
-                <th>時間</th>
-                <th>活動</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>19:50</td>
-                <td>發送 Google Meet 會議代碼</td>
-              </tr>
-              <tr>
-                <td>20:00-20:20</td>
-                <td>第一位講者</td>
-              </tr>
-              <tr>
-                <td>20:20-20:30</td>
-                <td>討論時間</td>
-              </tr>
-              <tr>
-                <td>20:30-20:50</td>
-                <td>第二位講者</td>
-              </tr>
-              <tr>
-                <td>20:50-21:00</td>
-                <td>討論時間</td>
-              </tr>
-              <tr>
-                <td>21:00</td>
-                <td>填寫回饋問卷，結束</td>
-              </tr>
-            </tbody>
-          </table>
+          <Title>注意事項</Title>
+          <div className="columns">
+            <InfoCard title="參與中" col={6}>
+              <ul>
+                <li>不發出其他聲響干擾演講者，如播音樂。</li>
+                <li>
+                  中途離席
+                  <span className="tag is-warning is-light is-medium px-0">不需</span>
+                  告知。
+                </li>
+              </ul>
+            </InfoCard>
+            <InfoCard title="參與後" col={6}>
+              <ul>
+                <li>
+                  填寫
+                  <span className="tag is-warning is-light is-medium px-0">回饋意見表單</span>
+                  ，感謝參與，連結將在結束後發送。
+                </li>
+                <li>報名擔任講者請洽 Niko。</li>
+              </ul>
+            </InfoCard>
+          </div>
+        </div>
+      </section>
+      {/* Agenda */}
+      <section className="hero is-primary is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <Title>議程</Title>
+            <InfoCard>
+              <table className="table" style={{ width: '100%' }}>
+                <thead>
+                  <tr>
+                    <th>時間</th>
+                    <th>活動</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>20:00</td>
+                    <td>發送 Google Meet 會議連結</td>
+                  </tr>
+                  <tr>
+                    <td>20:30-20:50</td>
+                    <td>第一位講者</td>
+                  </tr>
+                  <tr>
+                    <td>20:50-21:00</td>
+                    <td>討論時間</td>
+                  </tr>
+                  <tr>
+                    <td>21:00-21:20</td>
+                    <td>第二位講者</td>
+                  </tr>
+                  <tr>
+                    <td>21:20-21:30</td>
+                    <td>討論時間</td>
+                  </tr>
+                  <tr>
+                    <td>21:30</td>
+                    <td>填寫回饋問卷，結束</td>
+                  </tr>
+                </tbody>
+              </table>
+            </InfoCard>
+          </div>
         </div>
       </section>
       {/* Footer */}
--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-function InfoCard({ title, children }) {
+function InfoCard({ title, children, col = 12 }) {
   return (
-    <div className="column is-4" data-aos="fade-up">
+    <div className={`column is-${col}`} data-aos="fade-up">
       <div className="card">
-        <header className="card-header">
-          <p className="card-header-title">{title}</p>
-        </header>
+        {title && (
+          <header className="card-header">
+            <p className="card-header-title">{title}</p>
+          </header>
+        )}
         <div className="card-content">
           <div className="content">{children}</div>
         </div>
